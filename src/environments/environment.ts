da90d1354a5b7c63c2475dfe1276ebbb32a0a95b
Anna Melnychenko <88847909+annamelya2021@users.noreply.github.com>
2024-10-17 16:40:20 +0200

    commit

---
+++ src/environments/environment.ts
@@ -0,0 +1,8 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: false,
+  apiKey: 'pk.eyJ1IjoiYW5uYW1lbHlhIiwiYSI6ImNtMmRjcG5pNzByYjgyanNmbDR5OGJlYTgifQ.kaxEcGsAYdQVs71GKA8tOg',
+};
